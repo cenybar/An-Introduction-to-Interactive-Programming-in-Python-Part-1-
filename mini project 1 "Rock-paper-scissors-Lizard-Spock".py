1c2f641079654bbfa7f35c453445e9c0296ae63d
Santiago Fernandez <cenybar@gmail.com>
2019-07-10 13:28:08 +0200

    Add missing parenthesis in print statements

--- mini project 1 "Rock-paper-scissors-Lizard-Spock".py
+++ mini project 1 "Rock-paper-scissors-Lizard-Spock".py
@@ -42,9 +42,9 @@ def rpsls(player_choice):
     # delete the following pass statement and fill in your code below
         
     # print a blank line to separate consecutive games
-    print ""
+    print ("")
     # print out the message for the player's choice
-    print "Player chooses",player_choice
+    print ("Player chooses",player_choice)
     # convert the player's choice to player_number using the function name_to_number()
     player_number = name_to_number(player_choice)
     # compute random guess for comp_number using random.randrange()
@@ -53,16 +53,16 @@ def rpsls(player_choice):
     # convert comp_number to comp_choice using the function number_to_name()
     comp_name = number_to_name(comp_number)
     # print out the message for computer's choice
-    print "Computer chooses",comp_name
+    print ("Computer chooses",comp_name)
     # compute difference of comp_number and player_number modulo five
     modulo_five = (player_number - comp_number) % 5
     # use if/elif/else to determine winner, print winner message
     if modulo_five >= 3:
-        print "Computer wins!"
+        print ("Computer wins!")
     elif modulo_five >= 1:
-        print "Player wins!"
+        print ("Player wins!")
     else:
-        print "Player and computer tie!"
+        print ("Player and computer tie!")
 
 
     
